0579e943acb7aa5dae96d768d6a91fc58da7a1fa
Rand Black <braniganrobot@me.com>
2019-04-20 10:48:07 -0700

    init javascript file

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let name = null;
+let phoneNumber = null;
+let email = null;
+let uniqueId = null;
+
+function addReservation() {
+  var name = document.getElementById('name').value;
+  var phoneNumber = document.getElementById('phoneNumber').value;
+  var email = document.getElementById('email').value;
+  var uniqueId = document.getElementById('uniqueId').value;
+
+
+}
+
+
+
+// each variable is assigned to a column in a table and updated
